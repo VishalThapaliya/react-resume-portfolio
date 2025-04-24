4deea0353199992847ef6a064b8c9e498ed70f8f
VishalThapaliya <vishal.thapaliya@gmail.com>
2025-04-24 17:10:48 +0200

    add portfolio images

--- src/pages/portfolio/Portfolio.jsx
+++ src/pages/portfolio/Portfolio.jsx
@@ -2,15 +2,25 @@ import React, { useState } from 'react';
 import './Portfolio.css';
 
 // Images
-import financeImage from '../../assets/images/project-1.jpg';
-import orizonImage from '../../assets/images/project-2.png';
-import fundoImage from '../../assets/images/project-3.jpg';
-import brawlhallaImage from '../../assets/images/project-4.png';
-import dsmImage from '../../assets/images/project-5.png';
-import metaSparkImage from '../../assets/images/project-6.png';
-import summaryImage from '../../assets/images/project-7.png';
-import taskManagerImage from '../../assets/images/project-8.jpg';
-import arrivalImage from '../../assets/images/project-9.png';
+
+// web development
+import jonathanTejasImage from '../../assets/images/project-1.png';
+import katmandouImage from '../../assets/images/project-2.png';
+import himalayanImage from '../../assets/images/project-3.png';
+import bibiFlixImage from '../../assets/images/project-4.png';
+import adminDashboardImage from '../../assets/images/project-5.png';
+
+// web design
+import manomaImage from '../../assets/images/UI_Design_01.png';
+import artiersImage from '../../assets/images/UI_Design_02.png';
+import bibilonImage from '../../assets/images/UI_Design_03.png';
+import oculusImage from '../../assets/images/UI_Design_04.png';
+import bibiProductsImage from '../../assets/images/UI_Design_05.png';
+import bibiFitImage from '../../assets/images/UI_Design_06.png';
+import bibiPhoqueImage from '../../assets/images/UI_Design_07.png';
+import khaanaImage from '../../assets/images/UI_Design_08.png';
+import bibiGemImage from '../../assets/images/UI_Design_09.png';
+import wiseImage from '../../assets/images/UI_Design_10.png';
 
 // Filters with unique IDs
 const filterList = [
@@ -22,16 +32,23 @@ const filterList = [
 
 // Project Data
 const projects = [
-  { id: 1, image: financeImage, title: 'Finance', category: 'Web development' },
-  { id: 2, image: orizonImage, title: 'Orizon', category: 'Web development' },
-  { id: 3, image: fundoImage, title: 'Fundo', category: 'Web design' },
-  { id: 4, image: brawlhallaImage, title: 'Brawlhalla', category: 'Applications' },
-  { id: 5, image: dsmImage, title: 'DSM', category: 'Web design' },
-  { id: 6, image: metaSparkImage, title: 'MetaSpark', category: 'Web design' },
-  { id: 7, image: summaryImage, title: 'Summary', category: 'Web development' },
-  { id: 8, image: taskManagerImage, title: 'Task Manager', category: 'Applications' },
-  { id: 9, image: arrivalImage, title: 'Arrival', category: 'Web development' },
-  { id: 10, image: financeImage, title: 'Finance v2', category: 'Web development' },
+  { id: 1, image: jonathanTejasImage, title: 'Jonathan Tejas', category: 'Web development', link: '#'},
+  { id: 2, image: katmandouImage, title: 'Restaurant Katmandou', category: 'Web development', link: '#' },
+  { id: 3, image: manomaImage, title: 'Manoma', category: 'Web design', link: 'https://www.figma.com/proto/QdobAezB4s7WPoYz94u3hT/UI_Design_Challenges?node-id=1-15&t=56A3CCn9lLbXn3St-1' },
+  { id: 4, image: manomaImage, title: 'Brawlhalla', category: 'Applications', link: '#' },
+  { id: 5, image: artiersImage, title: 'Artiers', category: 'Web design', link: 'https://www.figma.com/proto/QdobAezB4s7WPoYz94u3hT/UI_Design_Challenges?node-id=13-2&t=56A3CCn9lLbXn3St-1' },
+  { id: 6, image: bibilonImage, title: 'Bibilon', category: 'Web design', link: 'https://www.figma.com/proto/QdobAezB4s7WPoYz94u3hT/UI_Design_Challenges?node-id=20-2&t=56A3CCn9lLbXn3St-1' },
+  { id: 7, image: himalayanImage, title: 'Himalayan Restaurant', category: 'Web development', link: '#' },
+  { id: 8, image: adminDashboardImage, title: 'Task Manager', category: 'Web development', link: '#' },
+  { id: 9, image: bibiFlixImage, title: 'BibiFlix Movies', category: 'Web development', link: '#' },
+  { id: 10, image: jonathanTejasImage, title: 'Finance v2', category: 'Web development', link: '#' },
+  { id: 11, image: oculusImage, title: 'Oculus', category: 'Web design', link: 'https://www.figma.com/proto/QdobAezB4s7WPoYz94u3hT/UI_Design_Challenges?node-id=33-34&t=56A3CCn9lLbXn3St-1' },
+  { id: 12, image: bibiProductsImage, title: 'Bibi Products', category: 'Web design', link: 'https://www.figma.com/proto/QdobAezB4s7WPoYz94u3hT/UI_Design_Challenges?node-id=47-2&t=56A3CCn9lLbXn3St-1' },
+  { id: 13, image: bibiFitImage, title: 'Bibifit', category: 'Web design', link: 'https://www.figma.com/proto/QdobAezB4s7WPoYz94u3hT/UI_Design_Challenges?node-id=52-2&t=56A3CCn9lLbXn3St-1' },
+  { id: 14, image: bibiPhoqueImage, title: 'Bibi Phoque', category: 'Web design', link: 'https://www.figma.com/proto/QdobAezB4s7WPoYz94u3hT/UI_Design_Challenges?node-id=67-121&t=56A3CCn9lLbXn3St-1' },
+  { id: 15, image: khaanaImage, title: 'Khaana', category: 'Web design', link: 'https://www.figma.com/proto/QdobAezB4s7WPoYz94u3hT/UI_Design_Challenges?node-id=81-4&t=56A3CCn9lLbXn3St-1' },
+  { id: 16, image: bibiGemImage, title: 'Bibi Gem', category: 'Web design', link: 'https://www.figma.com/proto/QdobAezB4s7WPoYz94u3hT/UI_Design_Challenges?node-id=99-22&t=56A3CCn9lLbXn3St-1' },
+  { id: 17, image: wiseImage, title: 'Wise', category: 'Web design', link: 'https://www.figma.com/proto/QdobAezB4s7WPoYz94u3hT/UI_Design_Challenges?node-id=114-13&t=56A3CCn9lLbXn3St-1' }
 ];
 
 const Portfolio = () => {
@@ -100,7 +117,7 @@ const Portfolio = () => {
         <ul className="project-list">
           {filteredProjects.map((project) => (
             <li className="project-item active" key={project.id}>
-              <a href="#">
+              <a href={project.link} target='_blank'>
                 <figure className="project-img">
                   <div className="project-item-icon-box">
                     <ion-icon name="eye-outline"></ion-icon>
